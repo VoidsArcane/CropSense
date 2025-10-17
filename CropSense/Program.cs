55381d7a024c503810b4ebe953cb3bad351242e3
John Psaroudakis <gpsaroudakis1001@gmail.com>
2025-10-17 07:46:49 +0300

    Add API Integration and JSON deserialization

--- CropSense/Program.cs
+++ CropSense/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+
+using HttpClient client = new HttpClient();
+
+string url = "https://api.open-meteo.com/v1/forecast?latitude=37.945&longitude=23.7142&daily=temperature_2m_max,temperature_2m_min,rain_sum,showers_sum,snowfall_sum,precipitation_sum,precipitation_hours,precipitation_probability_max,sunrise,sunset,daylight_duration,uv_index_max,wind_speed_10m_max,wind_gusts_10m_max,weather_code&timezone=auto";
+
+HttpResponseMessage response = await client.GetAsync(url);
+response.EnsureSuccessStatusCode();
+
+string json = await response.Content.ReadAsStringAsync();
+
+using JsonDocument doc = JsonDocument.Parse(json);
+JsonElement root = doc.RootElement;
+
+
+
+// Printing out the JSON
+string prettyJson = JsonSerializer.Serialize(root, new JsonSerializerOptions
+{
+	WriteIndented = true
+});
+
+Console.WriteLine(prettyJson);
