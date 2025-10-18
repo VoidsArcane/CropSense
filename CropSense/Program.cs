dc28a40fd7b60fde3d835715182cb4ace5e077df
John Psaroudakis <gpsaroudakis1001@gmail.com>
2025-10-18 13:11:02 +0300

    Refactor and Finalize V-1.0

--- CropSense/Program.cs
+++ CropSense/Program.cs
@@ -8,7 +8,7 @@ using System.Linq;
 // Fetching Data from API
 using HttpClient client = new HttpClient();
 
-string url = "https://api.open-meteo.com/v1/forecast?latitude=37.945&longitude=23.7142&daily=weather_code,temperature_2m_max,temperature_2m_min,rain_sum,showers_sum,snowfall_sum,precipitation_sum,precipitation_hours,precipitation_probability_max,sunrise,sunset,daylight_duration,sunshine_duration,uv_index_max,wind_gusts_10m_max,wind_speed_10m_mean,shortwave_radiation_sum,et0_fao_evapotranspiration,temperature_2m_mean,cloud_cover_mean,relative_humidity_2m_mean&timezone=auto";
+string url = "https://api.open-meteo.com/v1/forecast?latitude=37.945&longitude=23.7142&daily=temperature_2m_max,temperature_2m_min,rain_sum,snowfall_sum,precipitation_sum,wind_gusts_10m_max,shortwave_radiation_sum,et0_fao_evapotranspiration&timezone=auto";
 
 HttpResponseMessage response = await client.GetAsync(url);
 response.EnsureSuccessStatusCode();
@@ -19,21 +19,11 @@ string json = await response.Content.ReadAsStringAsync();
 using JsonDocument doc = JsonDocument.Parse(json);
 JsonElement root = doc.RootElement.GetProperty("daily");
 
-/*
-// Printing out the JSON (DEBUG)
-string prettyJson = JsonSerializer.Serialize(root, new JsonSerializerOptions
-{
-	WriteIndented = true
-});
-
-Console.WriteLine(prettyJson);
-*/
 // Generating Report
 StringBuilder stringBuilder = new StringBuilder();
 
 stringBuilder.Append("<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css\">");
-
-stringBuilder.Append("<table class=\"table table-striped table-hover table-bordered\"><thead class=\"table-dard\"><th>Date</th>");
+stringBuilder.Append("<table class=\"table table-striped table-hover table-bordered\"><thead class=\"table-dark\"><th>Date</th>");
 stringBuilder.Append("<th>Min Temp (°C)</th><th>Max Temp (°C)</th><th>Snow (mm)</th><th>Rain (mm)</th>");
 stringBuilder.Append("<th>ET₀ (mm)</th>");
 stringBuilder.Append("<th>Shortwave Radiation (MJ/m²)</th><th>Wind Gusts (km/h)</th>");
@@ -41,35 +31,33 @@ stringBuilder.Append("</thead><tbody>");
 
 for (int i = 0; i < root.GetProperty("time").EnumerateArray().Count(); i++)
 {
-	stringBuilder.Append("<tr>");
 	string? date = root.GetProperty("time").EnumerateArray().ElementAt(i).GetString();
-	stringBuilder.Append("<td>" + date + "</td>");
-
-	float tempMax = root.GetProperty("temperature_2m_max").EnumerateArray().ElementAt(i).GetSingle();
-	float tempMin = root.GetProperty("temperature_2m_min").EnumerateArray().ElementAt(i).GetSingle();
-	stringBuilder.Append("<td>" + tempMin + "</td>");
-	stringBuilder.Append("<td>" + tempMax + "</td>");
-
-	double snowfallSum = root.GetProperty("snowfall_sum").EnumerateArray().ElementAt(i).GetDouble();
-	stringBuilder.Append("<td>" + snowfallSum + "</td>");
-
-	double precipitationSum = root.GetProperty("precipitation_sum").EnumerateArray().ElementAt(i).GetDouble();
-	double rainSum = precipitationSum - snowfallSum;
-	stringBuilder.Append("<td>" + rainSum + "</td>");
-
+	float maxTemp = root.GetProperty("temperature_2m_max").EnumerateArray().ElementAt(i).GetSingle();
+	float minTemp = root.GetProperty("temperature_2m_min").EnumerateArray().ElementAt(i).GetSingle();
+	float windGusts = root.GetProperty("wind_gusts_10m_max").EnumerateArray().ElementAt(i).GetSingle();
+	double snow = root.GetProperty("snowfall_sum").EnumerateArray().ElementAt(i).GetDouble();
+	double totalPrecipitation = root.GetProperty("precipitation_sum").EnumerateArray().ElementAt(i).GetDouble();
+	double rain = totalPrecipitation - snow;
 	double evapotranspiration = root.GetProperty("et0_fao_evapotranspiration").EnumerateArray().ElementAt(i).GetDouble();
-	stringBuilder.Append("<td>" + evapotranspiration + "</td>");
+	double shortwaveRadiation = root.GetProperty("shortwave_radiation_sum").EnumerateArray().ElementAt(i).GetDouble();
 
-	double shortwaveRadiationSum = root.GetProperty("shortwave_radiation_sum").EnumerateArray().ElementAt(i).GetDouble();
-	stringBuilder.Append("<td>" + shortwaveRadiationSum + "</td>");
+	string minTempColor = minTemp <= 5 ? "color: red;" : minTemp <= 10 ? "color: orange;" : "";
+	string windGustColor = windGusts >= 50 ? "color: red;" : "";
 
-	float windGustsMax = root.GetProperty("wind_gusts_10m_max").EnumerateArray().ElementAt(i).GetSingle();
-	stringBuilder.Append("<td>" + windGustsMax + "</td>");
-
+	stringBuilder.Append("<tr>");
+	stringBuilder.Append($"<td>{date}</td>");
+	stringBuilder.Append($"<td style='{minTempColor}'> {minTemp} </td>");
+	stringBuilder.Append($"<td>{maxTemp}</td>");
+	stringBuilder.Append($"<td>{snow}</td>");
+	stringBuilder.Append($"<td>{rain}</td>");
+	stringBuilder.Append($"<td>{evapotranspiration}</td>");
+	stringBuilder.Append($"<td>{shortwaveRadiation}</td>");
+	stringBuilder.Append($"<td style='{windGustColor}'>{windGusts}</td>");
 	stringBuilder.Append("</tr>");
 }
-
 stringBuilder.Append("</tbody></table>");
+
+// Creating and Opening Report
 string reportFilePath = "report.html";
 System.IO.File.WriteAllText(reportFilePath, stringBuilder.ToString());
 
