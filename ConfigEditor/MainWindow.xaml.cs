c5f73b5183f5640913d54c09172b8eb57dfd21f2
John Psaroudakis <gpsaroudakis1001@gmail.com>
2025-10-29 13:50:05 +0200

    Finalize ConfigEditor Version 1.0

--- ConfigEditor/MainWindow.xaml.cs
+++ ConfigEditor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Windows.Forms;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,27 +11,132 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
-using System.Windows.Forms;
+using Application = System.Windows.Application;
+using CheckBox = System.Windows.Controls.CheckBox;
+
 
 namespace ConfigEditor;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
-	private Dictionary<string, Dictionary<string, string>> config;
+	string CONFIG_PATH = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.ini");
+	private Dictionary<string, string> generalSettings;
+	private Dictionary<string, string> variableSettings;
 
 	public MainWindow()
 	{
 		InitializeComponent();
 
-		// Parse Config
-		string iniPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.ini");
+		ParseConfig();
 
-		var lines = File.ReadAllLines(iniPath);
+		if (generalSettings.ContainsKey("location"))
+		{
+			location.Text = generalSettings["location"].Trim();
+		}
+
+		if (generalSettings.ContainsKey("forecast_days"))
+		{
+			if (int.TryParse(generalSettings["forecast_days"], out int ForecastDays))
+			{
+				forecast_days.SelectedIndex = (ForecastDays > 0) ? ForecastDays - 1 : 0;
+			}
+		}
+
+		if (generalSettings.ContainsKey("auto_open_report"))
+		{
+			if (bool.TryParse(generalSettings["auto_open_report"], out bool AutoOpenReport))
+			{
+				auto_open_report.IsChecked = AutoOpenReport;
+			}
+		}
+
+		if (generalSettings.ContainsKey("report_file_path"))
+		{
+			report_file_path.Text = generalSettings["report_file_path"].Trim();
+		}
+
+
+		if (generalSettings.ContainsKey("report_type"))
+		{
+			report_type.SelectedIndex = generalSettings["report_type"].Trim().ToUpper() == "CSV" ? 1 : 0;
+		}
+
+
+		foreach (KeyValuePair<string, string> entry in variableSettings)
+		{
+			CheckBox variable = (CheckBox)this.FindName(entry.Key);
+
+			if (variable != null)
+			{
+				if (bool.TryParse(entry.Value, out bool BoolValue))
+				{
+					variable.IsChecked = BoolValue;
+				}
+				else
+				{
+					variable.IsChecked = false;
+				}
+			}
+		}
+
+	}
+
+	private void SaveConfig(object sender, RoutedEventArgs e)
+	{
+		generalSettings["location"] = location.Text;
+		generalSettings["forecast_days"] = forecast_days.SelectedIndex + 1 + "";
+		generalSettings["auto_open_report"] = (auto_open_report.IsChecked + "").ToLowerInvariant();
+		generalSettings["report_file_path"] = report_file_path.Text;
+		generalSettings["report_type"] = report_type.SelectedIndex == 0 ? "HTML" : "CSV";
+
+		foreach (KeyValuePair<string, string> entry in variableSettings)
+		{
+			CheckBox variable = (CheckBox) FindName(entry.Key);
+			variableSettings[entry.Key] = variable.IsChecked != null ? ("" + variable.IsChecked).ToLowerInvariant() : "false";
+		}
+
+		StringBuilder stringBuilder = new StringBuilder();
+
+		stringBuilder.AppendLine("[general]");
+		foreach (KeyValuePair<string, string> setting in generalSettings)
+		{
+			stringBuilder.AppendLine($"{setting.Key} = {setting.Value}");
+		}
+
+
+		stringBuilder.AppendLine();
+		stringBuilder.AppendLine("[include_variables]");
+		foreach (KeyValuePair<string, string> setting in variableSettings)
+		{
+			stringBuilder.AppendLine($"{setting.Key} = {setting.Value}");
+		}
+
+		File.WriteAllText(CONFIG_PATH, stringBuilder.ToString());
+		System.Windows.MessageBox.Show ("Config Saved", "Saved", MessageBoxButton.OK, MessageBoxImage.None);
+	}
+
+	private void GetOutputPath(object sender, RoutedEventArgs e)
+	{
+		using var dialog = new FolderBrowserDialog();
+		dialog.Description = "Select a folder";
+
+		DialogResult result = dialog.ShowDialog();
+		if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+		{
+			report_file_path.Text = dialog.SelectedPath;
+		}
+	}
+
+	private void ParseConfig()
+	{
+		if (!File.Exists(CONFIG_PATH))
+		{
+			DisplayFatalErrorMessage("Config File not found in /dist folder");
+		}
+
+		var lines = File.ReadAllLines(CONFIG_PATH);
 		string section = "";
-		config = new Dictionary<string, Dictionary<string, string>>();
+		Dictionary<string, Dictionary<string, string>> config = [];
 
 		foreach (var line in lines)
 		{
@@ -47,69 +153,25 @@ public partial class MainWindow : Window
 			}
 		}
 
-		// Set UI Field Values to current Config Values
-		location.Text = config["general"]["location"];
-		forecast_days.SelectedIndex = int.Parse(config["general"]["forecast_days"]) - 1;
-		auto_open_report.IsChecked = bool.Parse(config["general"]["auto_open_report"]);
-		report_file_path.Text = config["general"]["report_file_path"];
-		report_type.SelectedIndex = config["general"]["report_type"].ToUpper() == "HTML" ? 0 : 1;
-
-		Dictionary<string, string> variables = config["include_variables"];
-
-		foreach (KeyValuePair<string, string> entry in variables)
+		if (!config.ContainsKey("general"))
 		{
-			System.Windows.Controls.CheckBox variable = (System.Windows.Controls.CheckBox)this.FindName(entry.Key);
-			variable.IsChecked = bool.Parse(entry.Value);
+			DisplayFatalErrorMessage("Config File doesn't contain 'general' section");
 		}
 
+		if (!config.ContainsKey("include_variables"))
+		{
+			DisplayFatalErrorMessage("Config File doesn't contain 'include_variables' section");
+		}
+
+		generalSettings = config["general"];
+		variableSettings = config["include_variables"];
 	}
 
-	private void SaveConfig(object sender, RoutedEventArgs e)
+	private void DisplayFatalErrorMessage(string ErrorMessage)
 	{
-		config["general"]["location"] = location.Text;
-		config["general"]["forecast_days"] = forecast_days.SelectedIndex + 1 + "";
-		config["general"]["auto_open_report"] = auto_open_report.IsChecked + "";
-		config["general"]["report_file_path"] = report_file_path.Text;
-		config["general"]["report_type"] = report_type.SelectedIndex == 0 ? "HTML" : "CSV";
-
-		Dictionary<string, string> variables = config["include_variables"];
-
-		foreach (KeyValuePair<string, string> entry in variables)
-		{
-			System.Windows.Controls.CheckBox variable = (System.Windows.Controls.CheckBox)this.FindName(entry.Key);
-			variables[entry.Key] = variable.IsChecked != null ? variable.IsChecked + "" : "false";
-		}
-
-		string iniPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.ini");
-		StringBuilder stringBuilder = new StringBuilder();
-
-		foreach (KeyValuePair<string, Dictionary<string, string>> section in config)
-		{
-			stringBuilder.AppendLine($"[{section.Key}]");
-			foreach (KeyValuePair<string, string> entry in config[section.Key])
-			{
-				stringBuilder.AppendLine($"{entry.Key} = {entry.Value}");
-			}
-			stringBuilder.AppendLine();
-		}
-
-		System.IO.File.WriteAllText(iniPath, stringBuilder.ToString());
+		System.Windows.MessageBox.Show (ErrorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+		Application.Current.Shutdown();
+		return;
 	}
-
-	private void GetOutputPath(object sender, RoutedEventArgs e)
-	{
-		using (var dialog = new FolderBrowserDialog())
-		{
-			dialog.Description = "Select a folder";
-
-			DialogResult result = dialog.ShowDialog();
-
-			if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
-			{
-				string folderPath = dialog.SelectedPath;
-				// Set folder path in your UI field
-				report_file_path.Text = folderPath;
-			}
-		}
-	}
+	
 }
